5cb0da7e7234b6fe3d5ebd7e61a7ef5898e8e161
Tashi Sherpa <sherpajack3@gmail.com>
2026-07-18 22:14:38 +0545

    Fixed case sensitivity issue in AuthController for deployement

--- Server/src/Controller/AuthController.js
+++ Server/src/Controller/AuthController.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
-import { registerSchema, loginSchema } from '../request/authRequest.js';
+import { registerSchema, loginSchema } from '../request/AuthRequest.js';
 
 // Helper to generate JWT token
 const generateToken = (user) => {
