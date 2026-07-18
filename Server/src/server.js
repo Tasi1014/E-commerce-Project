e81933dc20b2cef8b921375d63ea9edfdf41f8c0
Tashi Sherpa <sherpajack3@gmail.com>
2026-07-18 22:34:43 +0545

    Fixed the import Config conflict in server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'; 
 import app from './app.js';
-import { config } from './config/appConfig.js';
+import { config } from './Config/appConfig.js';
 import "./Config/mongodbConfig.js"
 
 const PORT = config.port;
